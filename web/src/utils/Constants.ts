302ebffa9858fa5a5d0636d42a86b9115f7895bc
David Price <thedavid@thedavidprice.com>
2023-09-06 08:19:33 -0700

    update GitHub Repo link

--- web/src/utils/Constants.ts
+++ web/src/utils/Constants.ts
@@ -1,7 +1,7 @@
 export const Constants = Object.freeze({
   ABOUT: 'https://www.redwoodjsconf.com/',
   SPEAKERS: 'https://www.redwoodjsconf.com/speakers',
-  GITHUB_URL: 'https://github.com/ahaywood/redwoodjs-partner-badges',
+  GITHUB_URL: 'https://github.com/redwoodjs/ticket-badge-app',
   DEFAULT_PARTNER_SLUG: 'redwoodjs',
   CALENDAR_URL: `${process.env.REDWOOD_ENV_BASE_URL}/files/RedwoodJSConference2023.ics`,
 })
