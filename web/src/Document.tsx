fc9cac5a88d96eaaf3396af0eaa4cd02c0f17a0e
Amy Haywood Dutton <me@amyhaywood.com>
2023-09-07 13:12:15 -0500

    ✨ Added Fathom Analytics code

--- web/src/Document.tsx
+++ web/src/Document.tsx
@@ -15,6 +15,12 @@ export const Document: React.FC<DocumentProps> = ({ children, css, meta }) => {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/png" href="/favicon.png" />
+        <script
+          src="https://cdn.usefathom.com/script.js"
+          data-spa="auto"
+          data-site="UPFFRZZI"
+          defer
+        ></script>
         <Css css={css} />
         <Meta tags={meta} />
       </head>
