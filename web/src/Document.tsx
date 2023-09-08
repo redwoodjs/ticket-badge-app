f9a20c0a432bbab64c4add5a5eea6065aabe2858
Amy Haywood Dutton <me@amyhaywood.com>
2023-09-07 20:30:54 -0500

    🤪 Testing Meta Tags

--- web/src/Document.tsx
+++ web/src/Document.tsx
@@ -23,6 +23,55 @@ export const Document: React.FC<DocumentProps> = ({ children, css, meta }) => {
         ></script>
         <Css css={css} />
         <Meta tags={meta} />
+        <title data-rwjs-head="true">
+          Tom Preston-Werner Invites you to the RedwoodJS Conference
+        </title>
+        <meta
+          property="og:title"
+          content="Tom Preston-Werner Invites you to the RedwoodJS Conference"
+          data-rwjs-head="true"
+        />
+        <meta
+          property="twitter:title"
+          content="Tom Preston-Werner Invites you to the RedwoodJS Conference"
+          data-rwjs-head="true"
+        />
+        <meta
+          name="description"
+          content="Join Tom Preston-Werner at the RedwoodJS Conference! Get a free virtual ticket or 35% off an in-person pass. Experience the latest in web development and connect with leading experts. Register now for this must-attend event."
+          data-rwjs-head="true"
+        />
+        <meta
+          name="twitter:description"
+          content="Join Tom Preston-Werner at the RedwoodJS Conference! Get a free virtual ticket or 35% off an in-person pass. Experience the latest in web development and connect with leading experts. Register now for this must-attend event."
+          data-rwjs-head="true"
+        />
+        <meta
+          property="og:description"
+          content="Join Tom Preston-Werner at the RedwoodJS Conference! Get a free virtual ticket or 35% off an in-person pass. Experience the latest in web development and connect with leading experts. Register now for this must-attend event."
+          data-rwjs-head="true"
+        />
+        <meta
+          property="og:url"
+          content="https://ticket.redwoodjs.com/invite/undefined"
+          data-rwjs-head="true"
+        />
+        <meta property="og:type" content="website" data-rwjs-head="true" />
+        <meta
+          property="og:image"
+          content="http://res.cloudinary.com/duh8p234y/image/upload/v1694126172/partner-badges/badges/urltoimage_btrs0r.jpg"
+          data-rwjs-head="true"
+        />
+        <meta
+          property="twitter:card"
+          content="summary_large_image"
+          data-rwjs-head="true"
+        />
+        <meta
+          property="twitter:image"
+          content="http://res.cloudinary.com/duh8p234y/image/upload/v1694126172/partner-badges/badges/urltoimage_btrs0r.jpg"
+          data-rwjs-head="true"
+        />
       </head>
       <body>
         <div id="redwood-app">{children}</div>
